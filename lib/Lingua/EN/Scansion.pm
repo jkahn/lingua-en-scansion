1db788bf89cbd6a7f2c6ae28ef26b38e876a68bd
Jeremy G. Kahn <jgk@u.washington.edu>
2009-03-04 17:54:16 -0800

    first checking of Lingua::EN::Scansion files

---
+++ lib/Lingua/EN/Scansion.pm
@@ -0,0 +1,158 @@
+package Lingua::EN::Scansion;
+
+use strict;
+use warnings;
+use Carp;
+
+sub pretty_print {
+  my $class = shift;
+  my %args = @_;
+  my $title = $args{title};
+  my @formatted;
+  for my $line (@{$args{lines}}) {
+    my @words = map { $_->word } @{$line};
+    $words[0] = ucfirst $words[0];
+    push @formatted, join " ", @words;
+  }
+
+  use List::Util 'max';
+  my $divider = '-' x max (map { length $_ } @formatted);
+  return join ("\n", $title, $divider, @formatted);
+}
+
+sub lines_by_syllcts {
+  my $class = shift;
+  my %args = @_;
+
+  my @syllcts = @{$args{syllcts}};
+  my @words = @{$args{words}};
+
+  my @lines;
+  my $overrun = 0;
+  for my $len (@syllcts) {
+    my @line;
+    my $so_far = $overrun; $overrun = 0;
+    while ($so_far < $len) {
+      my $next_wd = shift @words;
+      my $ct = scalar $next_wd->sylls();
+      if ($so_far + $ct > $len) {
+	$overrun = $so_far + $ct - $len;
+	warn "line-break needed in " . $next_wd->debug . "\n"
+	  if $args{verbose};
+      }
+      push @line, $next_wd;
+      $so_far += $ct;
+    }
+    push @lines, \@line;
+  }
+  return @lines;
+}
+
+package Lingua::EN::Scansion::Word;
+
+use strict;
+use warnings;
+use Carp;
+
+# use overload '""' => \&stringify;
+
+sub new {
+  my $class = shift;
+  my $word = shift;
+
+
+  my @sylls =
+    Lingua::EN::Scansion::Syllable->syllabify(word => $word);
+  return bless { sylls => \@sylls,
+		 word => $word }, $class;
+}
+
+sub word {
+  my $self = shift;
+  return $self->{word};
+}
+
+sub sylls {
+  my $self = shift;
+  return @{$self->{sylls}};
+}
+
+sub debug {
+  my $self = shift;
+  return join "~", $self->sylls();
+}
+
+
+package Lingua::EN::Scansion::Syllable;
+use strict;
+use warnings;
+use Carp;
+
+our $VERBOSE = 1;
+
+use overload '""' => \&stringify;
+
+use Lingua::EN::Phoneme;
+my $LEP = Lingua::EN::Phoneme->new();
+
+use Text::Hyphen;
+my $HYPHENATOR = Text::Hyphen->new();
+
+sub syllabify {
+  my $class = shift;
+  my %args = @_;
+
+  my $word = $args{word};
+
+  $word =~ s/\p{isPunctuation}+$//;
+
+  return () if $word =~ /^\s*$/;
+
+#   use Lingua::EN::Hyphenate 'syllables';
+#   my @spelled_sylls = syllables($word);
+
+  my @spelled_sylls = split /-/, $HYPHENATOR->hyphenate($word);
+
+  my @phonemes = $LEP->phoneme($word);
+  my @vowels = grep {/[012]$/} @phonemes;
+
+  if (not @phonemes) {
+    warn "no phonemes for $args{word}\n";
+    return
+      map { $class->new(spelled => $_) }
+	@spelled_sylls;
+  }
+
+  # otherwise we have phonemes
+  if (@vowels != @spelled_sylls and $VERBOSE) {
+    warn "mismatch between #vowels (" . scalar @vowels .
+      ") and #spelled_sylls " . join ('~', @spelled_sylls) . " in $word\n";
+  }
+
+  # trust the vowels more.  TO DO: better alignments.
+  my @out;
+  while (@vowels) {
+    my $vowel = shift @vowels;
+    my $spelled = shift @spelled_sylls;
+    if (not defined $spelled) {
+      $spelled = '';
+    }
+    if (@spelled_sylls and not @vowels) {
+      $spelled .= join ('', @spelled_sylls);
+    }
+    push @out, $class->new(spelled => $spelled, vowel => $vowel);
+  }
+  return @out;
+}
+
+sub new {
+  my $class = shift;
+  my %args = @_;
+  return bless \%args, $class;
+}
+sub stringify {
+  my $self = shift;
+  return $self->{spelled};
+}
+
+1;
